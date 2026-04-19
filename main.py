422721a2e55340376894743ac8e80450b3610222
KahKarol <karolinerodriguesferreira2005@gmail.com>
2026-04-19 05:02:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-from fastapi import FastAPI
-import random
+def saudacao():
+    return "Olá, GitHub Actions!"
 
-app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/testei")
-async def funcaoteste():
-    return {"teste": True, "num_aleatorio": random.randint(a=0, b=2000)} 
-
+if __name__ == "__main__":
+    print(saudacao())
