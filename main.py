1c0df2b1bc768341e19c534d51ccc70e96b0fbe4
KahKarol <karolinerodriguesferreira2005@gmail.com>
2026-04-20 09:36:01 -0300

    Atualização

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-"""Modulo principal do projeto para validacao no GitHub Actions."""
-import random
+"""Projeto no GitHub Actions."""
 from fastapi import FastAPI
-
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    """Rota raiz que retorna saudacao."""
-    return {"message": "Hello World"}
-
-@app.get("/testei")
-async def funcaoteste():
-    """Rota de teste que retorna um numero aleatorio."""
-    return {"teste": True, "num_aleatorio": random.randint(0, 1000)}
+    """Titulo"""
+    return {"message": "Meu primeiro Projeto com Docker!"}
 
